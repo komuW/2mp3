a0dcfceaf14f7d7fda21256985f29c836d56f675
komu <komuw05@gmail.com>
2015-01-14 09:51:46 +0300

    add option to erase original file

--- 2mp3/core.py
+++ 2mp3/core.py
@@ -7,9 +7,13 @@ The main module that converts your files to awesome mp3's.
 """
 
 import envoy
+from sys import argv, exit
 
 
 cwd = None
+
+if len(argv) > 1:
+    cwd = argv[1]
 
 # check whether ffmpeg is installed
 ffmpeg = envoy.run('ffmpeg')
@@ -20,23 +24,24 @@ if 'No such file or directory' in ffmpeg.std_err:
                      Dont worry, 2mp3 will install it in a giffy").std_out
     print out
 
-    y1 = envoy.run('sudo apt-add-repository -y ppa:jon-severinsson/ffmpeg') 
-    y2 = envoy.run('sudo apt-get -y update')
-    y3 = envoy.run('sudo apt-get -y install ffmpeg')
+    envoy.run('sudo apt-add-repository -y ppa:jon-severinsson/ffmpeg') 
+    envoy.run('sudo apt-get -y update')
+    envoy.run('sudo apt-get -y install ffmpeg')
 
     out = "\n ffmpeg has been succesfully installed. continuing with conversion...\n"
     print out
 
+# cwd = '/vagrant/2mp3/test_songs'
+list_all_in_dir = envoy.run('ls', cwd=cwd)
+std_out = list_all_in_dir.std_out
 
+if 'No such file or directory' in list_all_in_dir.std_err:
+    print "the directory you provided does not exist."
+    print "please provide a valid directory and rerun the command"
+    exit()
 
-the_cmd = 'for i in *.*; do ffmpeg -i "$i" -f mp3 "$i".mp3; done'
-convert = envoy.run(the_cmd)
-
-cwd = '/vagrant/2mp3/test_songs'
-list_all_in_dir = envoy.run('ls', cwd=cwd).std_out
-split_em = list_all_in_dir.split('\n')
-# remove empty items from list
-split_em = filter(None, split_em)
+split_em = std_out.split('\n')
+split_em = filter(None, split_em) # remove empty items from list
 
 print "BEGINNING CONVERSION, this may take a while.\n"
 for song in split_em:
@@ -44,7 +49,10 @@ for song in split_em:
     print "CONVERTING: {0} : INTO AN MP3 FILE...".format(song)
     convert = envoy.run(the_cmd, cwd=cwd)
 
-import pdb;pdb.set_trace()
+    if argv[2] and argv[2] == '-r':
+        x = envoy.run('rm -f "{0}"'.format(song), cwd=cwd) # remove original file
+
+#import pdb;pdb.set_trace()
 print "ok"
 print "ok"
 print "ok"
